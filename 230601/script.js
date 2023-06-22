f09c439cf2a5600f6fbdf49eb7a1c8599fa9c336
youngsoAN <127369546+youngsoAN@users.noreply.github.com>
2023-06-22 17:54:22 +0900

    Add files via upload

---
+++ 230601/script.js
@@ -0,0 +1,8 @@
+// 객체.메서드(매개변수);
+// object.method(parameter);
+console.log("메시지 출력"); // 콘솔 영역에 출력
+console.debug("log와 마찬가지로 콘솔 영역에 출력");
+console.clear();
+
+// 문서에 hello JavaScript를 작성
+document.querySelector("body").innerHTML = "Hello JavaScript";
